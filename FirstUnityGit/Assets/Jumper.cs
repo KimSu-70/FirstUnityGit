8584e2d2bbee38fd73613dcee38d618453f43377
KimSu-70 <rlatngh4770@naver.com>
2024-08-22 16:06:06 +0900

    feat : 메시지 함수 동작 확인

--- FirstUnityGit/Assets/Jumper.cs
+++ FirstUnityGit/Assets/Jumper.cs
@@ -8,10 +8,16 @@ public class Jumper : MonoBehaviour
 
     public Rigidbody rigid;
 
+    private void Awake()
+    {
+        Debug.Log("Awake 오브젝트 초기화!");
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start 부족한 부분 마저 초기화 작업!");
     }
 
     // Update is called once per frame
@@ -20,6 +26,22 @@ public class Jumper : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            Debug.Log("Update 점프 정상 동작 확인!");
         }
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable 활동시작!");
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("OnDisable 활동 끝!");
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("OnDestroy 역할 마치고 소실!");
     }
 }
